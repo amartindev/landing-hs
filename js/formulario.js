b499ae0c5c5d427a6671daec2dcaafc1b450aabb
Antonio Martin <luis.martinf89@gmail.com>
2023-07-18 13:08:38 -0500

    input form

--- js/formulario.js
+++ js/formulario.js
@@ -1,19 +1,22 @@
 (function () {
-    'use strict';
-  
-    // Obtiene todos los formularios a los que queremos aplicar la validación de Bootstrap
-    const forms = document.querySelectorAll('.needs-validation');
-  
-    // Itera sobre los formularios y previene el envío si no son válidos
-    Array.from(forms).forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-  
-        form.classList.add('was-validated');
-      }, false);
-    });
-  })();
-  
+  'use strict';
+
+  // Obtiene todos los formularios a los que queremos aplicar la validación de Bootstrap
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Itera sobre los formularios y previene el envío si no son válidos
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        // El formulario es válido, redirigir a gracias.html
+        window.location.href = 'https://google.com';
+
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
