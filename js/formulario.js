90f254627b5b9d95002192eec1022bf38b10a51d
Antonio Martin <luis.martinf89@gmail.com>
2023-07-19 11:18:37 -0500

    datos de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -7,21 +7,21 @@
   // Itera sobre los formularios y previene el envío si no son válidos
   Array.from(forms).forEach(function (form) {
     form.addEventListener('submit', function (event) {
-      if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      } else {
-        // Imprimir los datos del formulario en la consola
-        console.log("Datos del formulario:");
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (form.checkValidity()) {
+        // El formulario es válido
+        console.log('Formulario válido. Datos del formulario:');
         const formData = new FormData(form);
         formData.forEach((value, key) => {
-          console.log(key + ': ' + value);
+          console.log(key, ':', value);
         });
+        console.log(formData);
 
-        // El formulario es válido, redirigir a gracias.html
-        window.location.href = 'gracias.html';
-        event.returnValue = false;
-        
+        setTimeout(function () {
+          window.location.href = 'gracias.html';
+        }, 0);
       }
 
       form.classList.add('was-validated');
